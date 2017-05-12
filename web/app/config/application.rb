e8897d00ecc10bf89491d4b00b101776de5e9c0b
Nitescu Lucian <nitesculuci@gmail.com>
2017-05-12 19:26:46 +0200

    Http -> Https
    Http -> Https

--- web/app/config/application.rb
+++ web/app/config/application.rb
@@ -18,6 +18,7 @@ module VulnerableScanManager
 		# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 		# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 		# config.i18n.default_locale = :de
+		config.force_ssl = true
 		config.autoload_paths += %W(#{config.root}/lib)
 		config.assets.precompile += %w( filterrific/filterrific-spinner.gif )
 		# Do not swallow errors in after_commit/after_rollback callbacks.
