b7b55ca59fbd7af10a8df3edecd53084d0edc1b6
Nitescu Lucian <nitesculuci@gmail.com>
2017-05-24 22:49:19 +0300

    Http -> Https

--- web/app/config/application.rb
+++ web/app/config/application.rb
@@ -28,5 +28,6 @@ module VulnerableScanManager
 			g.test_framework :rspec,fixtures: true,view_specs: false,helper_specs: false,routing_specs: false,controller_specs: true,request_specs: false
 			g.fixture_replacement :factory_girl, dir: "spec/factories"
 		end
+		config.force_ssl = true
 	end
 end
