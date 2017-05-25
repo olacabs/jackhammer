37a5039f64941b1dded471dc24fa57f65974be1b
Nitescu Lucian <nitesculuci@gmail.com>
2017-05-25 22:20:10 +0300

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
 docker-compose build
 sleep 30
+docker-compose run --rm -e COMMON_NAME=Jackhammer -e KEY_NAME=Jackhammer -v /var/certs:/certs centurylink/openssl
 docker-compose run web rake db:create
 docker-compose run web rake db:migrate
 docker-compose run  -e APPLICATION_MODE=$1 web rake db:seed
