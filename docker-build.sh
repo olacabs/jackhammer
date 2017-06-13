9c9afaad6577e8c06faac3bf85ac588a33ea6ace
Daigo Sakamoto <daigo@treasure-data.com>
2017-06-13 10:28:06 +0900

    Wait MySQL initialization
    * Wait MySQL initialization
    * Reduce unnecessary container creation
    * Remove redundant definition of RAILS_ENV=production
    * Remove database migration from command in docker-conatiner.xml

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 docker-compose build
 docker-compose up -d
-docker-compose run web rake db:create
-docker-compose run web rake db:migrate
-docker-compose run  -e APPLICATION_MODE=$1 web rake db:seed
-docker-compose run web figlet -k -w 500 "JACKHAMMER IS UP" 
+docker-compose exec mysqldb sh -c 'while ! mysqladmin ping --silent; do sleep 1; done'
+docker-compose exec web rake db:migrate
+docker-compose run --rm -e APPLICATION_MODE=$1 web rake db:seed
+docker-compose exec web figlet -k -w 500 "JACKHAMMER IS UP"
