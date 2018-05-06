4bf6d1fd7c16dfc2c25731923b5e29ba8b19836a
Mohammad Zeeshan <zee7han@gmail.com>
2018-05-06 09:38:19 +0530

    Update the docker-build.sh
    This will remove error of  "installing the mocha '~> 0.14.0'  in your gemfile"

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 docker-compose build
 docker-compose up -d
-docker-compose run web rake db:create
-docker-compose run web rake db:migrate
-docker-compose run  -e APPLICATION_MODE=$1 web rake db:seed
+docker-compose run web bundle exec rake db:create
+docker-compose run web bundle exec rake db:migrate
+docker-compose run web bundle exec rake db:seed
 docker-compose run web figlet -k -w 500 "JACKHAMMER IS UP" 
